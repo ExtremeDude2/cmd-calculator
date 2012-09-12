3fe50cedb8eac3097e51cd5e734b074a17aef038
ExtremeDude2@gmail.com <ExtremeDude2@gmail.com@46ba8ff2-5545-019a-451e-843ceee3ad17>
2012-09-12 20:27:35 +0000

    Deh initial rev :3

---
+++ Calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while(true)
+	{
+		double num1, num2, add, sub, multi, div;
+		cout << "Please enter a number: ";
+		cin >> num1;
+		cout << "Please enter another number: ";
+		cin >> num2;
+		add = num1 + num2;
+		sub = num1 - num2;
+		multi = num1 * num2;
+		div = num1 / num2;
+		cout << num1 << " + " << num2 << " = " << add << endl;
+		cout << num1 << " - " << num2 << " = " << sub << endl;
+		cout << num1 << " * " << num2 << " = " << multi << endl;
+		if ( num2 == 0 )
+			cout << "Error Cannot divide by 0" << endl;
+		else
+		{
+			num2 != 0;
+			cout << num1 << " / " << num2 << " = " << div << endl;
+		}
+	}
+}
