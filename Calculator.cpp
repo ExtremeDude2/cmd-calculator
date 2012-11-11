004be07b24a00cc63492445c40624a2504f0bf73
ExtremeDude2@gmail.com <ExtremeDude2@gmail.com@46ba8ff2-5545-019a-451e-843ceee3ad17>
2012-11-11 18:07:10 +0000

    Code clean up and comments :D

--- Calculator.cpp
+++ Calculator.cpp
@@ -12,22 +12,22 @@ int main()
 		cin >> num1;
 		cout << "Please enter another number: ";
 		cin >> num2;
-		add = num1 + num2;
-		sub = num1 - num2;
-		multi = num1 * num2;
-		div = num1 / num2;
-		root1 = sqrt(num1);
-		root2 = sqrt(num2);
-		power = pow(num1, num2);
+		add = num1 + num2;										// Add first and second number
 		cout << num1 << " + " << num2 << " = " << add << endl;
+		sub = num1 - num2;										// Subtract first and second number
 		cout << num1 << " - " << num2 << " = " << sub << endl;
+		multi = num1 * num2;									// Multiply first and second number
 		cout << num1 << " * " << num2 << " = " << multi << endl;
+		div = num1 / num2;										// Divide first and second number
 		if ( num2 == 0 )
-			cout << "Error Cannot divide by 0" << endl;
+			cout << "Error Cannot divide by 0" << endl;			// Make sure not to divide by zero, also prevents crashing
 		else
 			cout << num1 << " / " << num2 << " = " << div << endl;
+		root1 = sqrt(num1);										// Square root of first number
 		cout << "The square root of " << num1 << " is " << root1 << endl;
+		root2 = sqrt(num2);										// Square root of second number
 		cout << "The square root of " << num2 << " is " << root2 << endl;
+		power = pow(num1, num2);								// First number to the power of the second number
 		cout << num1 << " to the power of " << num2 << " is " << power << endl;
 	}
 }
